edaa7b01f56ca87f229c737b242b6f52a8946730
christliu <215688426@qq.com>
2020-09-20 15:47:19 +0800

    add premake and dllexport/dllimport framework

---
+++ PreTest/Application/TEngine/src/TEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace TEngine
+{
+
+int __declspec(dllexport) Add(int a, int b);
+
+};
---
+++ PreTest/Application/TEngine/src/math.cpp
@@ -0,0 +1,9 @@
+#include "TEngine.h"
+
+namespace TEngine {
+
+	int Add(int a, int b)
+	{
+		return a + b;
+	}
+}
